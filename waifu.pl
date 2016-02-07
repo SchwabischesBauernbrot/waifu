43e65e65d2f9f7b6b0509980d80dbcbde45b97d9
Skylar Akemi <homu@homu.us>
2016-02-07 00:08:06 -0600

    Shortened file names for comfier links

--- waifu.pl
+++ waifu.pl
@@ -2,12 +2,12 @@
 #
 #	Waifu shrine
 #	Lists jpg, png, and gif files in a directory, builds an html page to display them
-#	https://github.com/homura/waifu
+#	https://gitgud.io/homu/waifu
 #
 use List::Util 'shuffle';
 use strict;
 
-my $title = "Homuhomu~";	# Page title
+my $title = "Homura Akemi ｢暁美 ほむら｣";	# Page title
 my $favicon = "../i/HomuraSoulGemTransformed.ico";	# Path to favicon
 
 
@@ -23,12 +23,12 @@ print html '</head>' . "\n";
 print html '<body>' . "\n";
 print html '<div id="photos">' . "\n";
 
-system('mkdir thumb');	# Creates a thumb directory if it doesn't already exist, preventing errors later on.
+system('mkdir t');	# Creates a thumb directory if it doesn't already exist, preventing errors later on.
 
 my @waifus;
-my @lsjpg = qx/ls -1 src\/*.jpg/;
-my @lspng = qx/ls -1 src\/*.png/;
-my @lsgif = qx/ls -1 src\/*.gif/;
+my @lsjpg = qx/ls -1 s\/*.jpg/;
+my @lspng = qx/ls -1 s\/*.png/;
+my @lsgif = qx/ls -1 s\/*.gif/;
 
 unshift (@waifus, @lsjpg);
 unshift (@waifus, @lspng);
@@ -36,13 +36,13 @@ unshift (@waifus, @lsgif);
 @waifus = shuffle(@waifus);	# Comment out this line to display waifus in numeric order (0-F) by their SHA1 hash.
 foreach (@waifus) {
 	chomp($_);
-	$_ =~ s/src\///;
-	my @sha1 = qx/sha1sum src\/$_/;
+	$_ =~ s/s\///;
+	my @sha1 = qx/sha1sum s\/$_/;
 	$sha1[0] =~ m/\.(\w{3})/;
-	my $filename = substr($sha1[0], 0, 40) . "." . $1;
-	system('mv src/' . $_ . ' ' . 'src/' . $filename);
-	system('convert src/' . $filename . ' -resize 400 thumb/' . $filename);
-	print html '<a href="./src/' . $filename . '"><img src="./thumb/' . $filename . '" alt="waifu"></a>' . "\n";
+	my $filename = substr($sha1[0], 0, 12) . "." . $1;
+	system('mv s/' . $_ . ' ' . 's/' . $filename);
+	system('convert s/' . $filename . ' -resize 400 t/' . $filename);
+	print html '<a href="./s/' . $filename . '"><img src="./t/' . $filename . '" alt="waifu"></a>' . "\n";
 }
 
 print "Found " . scalar(@waifus) . " waifus: " . scalar(@lsjpg) . " jpgs, " . scalar(@lspng) . " pngs, " . scalar(@lsgif) . " gifs\n";
